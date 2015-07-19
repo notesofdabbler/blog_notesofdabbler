63b6c0a05b613db929d14bd557f9a58345088876
rdabbler <dabbler110@gmail.com>
2015-07-19 11:02:23 -0400

    working on extracting affilation of authors

--- exploreAICHE2015_techProgram/scrapeData.R
+++ exploreAICHE2015_techProgram/scrapeData.R
@@ -60,11 +60,14 @@ for(i in 1:length(sessions)){
     print(i)
 }
 talksdf = bind_rows(talksdfL)
+talksdf$id = seq(1,nrow(talksdf))
+
 # conversion of accents and tildes in names
 # helpful link at http://stackoverflow.com/questions/9511281/handling-special-characters-e-g-accents-in-r
 #
-talksdf$author = iconv(talksdf$author,"UTF-8","latin1")
-talksdf$id = seq(1,nrow(talksdf))
+talksdf$author2 = iconv(talksdf$author,"UTF-8","latin1")
+talksdf$author3 = ifelse(is.na(talksdf$author2),talksdf$author,talksdf$author2)
+
 
 # get individual author info for each talk
 
@@ -75,7 +78,7 @@ splitauth = function(auth){
    return(auth3)
 }
 
-authL = lapply(talksdf$author, function(x) splitauth(x))
+authL = lapply(talksdf$author3, function(x) splitauth(x))
 
 authdfL = list()
 for(i in 1:length(authL)){
@@ -84,21 +87,60 @@ for(i in 1:length(authL)){
 }
 authdf = bind_rows(authdfL)
 
+# check authors where encoding doesn't work
+tmpqc = authdf
+tmpqc$author2 = iconv(tmpqc$author,"UTF-8","latin1")
+tmpqc2 = tmpqc[is.na(tmpqc$author2),]
+unique(tmpqc2$author)
+
+# One name appears multiple times due to encoding issues. Manually fix it
+authdf$author2 = ifelse(authdf$author == "Salvador García-MuÃ±oz","Salvador García-Muñoz",authdf$author)
+
 # number of talks by author
-authtalks = authdf %>% group_by(author) %>% summarize(numtalks = n()) %>% arrange(desc(numtalks))
+authtalks = authdf %>% group_by(author2) %>% summarize(numtalks = n()) %>% arrange(desc(numtalks))
+head(data.frame(authtalks),30)
+
+authtalks %>% group_by(numtalks) %>% summarize(numauth = n())
 
 xtauthinfo = character()
+abstract = character()
 for(i in 1:nrow(talksdf)){
-  xtauthinfo[i] = read_html(talksdf$talkurl[i]) %>% html_nodes(".paperauthors") %>% html_text()
+  doc = read_html(talksdf$talkurl[i])
+  xtauthinfo[i] =  doc %>% html_nodes(".paperauthors") %>% html_text()
+  getabstract = doc %>% html_nodes(".abstract") %>% html_text()
+  if(length(getabstract) == 1){
+      abstract[i] = getabstract
+  } else {
+      abstract[i] = ""
+    }
   print(i)
 }
 
+talkdetaildf = data.frame(xtauthinfo = xtauthinfo, abstract = abstract)
+talkdetaildf$id = seq(1:nrow(talkdetaildf))
+
 xtauthdfL = list()
 for(i in 1:length(xtauthinfo)){
-  xtauthinfo2 = str_split(xtauthinfo[i],",")
+  xtauthinfo1 = gsub(" and ",",",xtauthinfo[i])
+  xtauthinfo2 = str_split(xtauthinfo1,",")
   xtauthinfo3 = str_trim(xtauthinfo2[[1]])
   xtauthinfo4 = gsub("[0-9)(]","",xtauthinfo3)
   xtauthdfL[[i]] = data.frame(xtauthor = xtauthinfo4)
   xtauthdfL[[i]]$id = i
 }
 xtauthdf = bind_rows(xtauthdfL)
+xtauthdf$xtauthor2 = iconv(xtauthdf$xtauthor,"UTF-8","latin1")
+
+# check authors where encoding doesn't work
+tmpqc = xtauthdf[is.na(xtauthdf$xtauthor2),]
+unique(data.frame(tmpqc$xtauthor))
+
+# One name appears multiple times due to encoding issues. Manually fix it
+xtauthdf$xtauthor2 = ifelse(xtauthdf$xtauthor == "Salvador García-MuÃ±oz","Salvador García-Muñoz",xtauthdf$xtauthor)
+
+
+# save datasets
+save(talksdf,file = "talksdf.Rda")
+save(authdf,file = "authdf.Rda")
+save(talkdetaildf,file = "talkdetaildf.Rda")
+save(xtauthdf,file = "xtauthdf.Rda")
