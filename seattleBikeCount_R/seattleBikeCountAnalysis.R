f01a90b258266e5ec57dd7701e455d9dbc66295b
rdabbler <dabbler110@gmail.com>
2015-08-02 08:04:14 -0400

    worked more on the bicycle count code

--- seattleBikeCount_R/seattleBikeCountAnalysis.R
+++ seattleBikeCount_R/seattleBikeCountAnalysis.R
@@ -3,15 +3,18 @@
 # Jake Vanderplas recently wrote a very nice article on Learning Seattle Work habits from bicycle counts
 #  https://jakevdp.github.io/blog/2015/07/23/learning-seattles-work-habits-from-bicycle-counts/
 #
-# I was trying to see if I can replicate the analysis in R
+# I was trying to see if I can work through that analysis using R
 #
 
 # load libraries
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(lubridate)
-library(mclust)
+library(dplyr) # data munging
+library(tidyr) # data munging
+library(ggplot2) # plotting
+library(lubridate) # work with dates
+library(mclust) # for clustering using gaussian mixture model
+library(Cairo) # for getting png outputs
+library(rje) # to use cubehelix colors for plots
+library(timeDate) # get list of US holidays
 
 
 # load data
@@ -40,11 +43,15 @@ rawcounts = rawcounts %>% group_by(Date) %>% mutate(hr = seq(1,length(Date)))
 head(rawcounts)
 
 # sum by week and plot trend
+# In Python with Pandas in Jake's post, from data set to this plot was a single line of code 
+# some of the things I am doing on date processing might have better ways to code
 rawcounts_wk = rawcounts %>% group_by(Weekchr) %>% summarize(West = sum(West), East = sum(East), total = sum(total))
 rawcounts_wk$Week = as.Date(rawcounts_wk$Weekchr)
 rawcounts_wk2 = gather(rawcounts_wk,var,val,-Week,-Weekchr)
 
+CairoPNG("bikecounts_wk.png",width = 600, height = 480)
 ggplot(rawcounts_wk2) + geom_line(aes(x = Week,y = val,color = var)) + xlab("Date") + ylab("Weekly Trips") + theme_bw()
+dev.off()
 
 # spread data so that each row is a day and columns are bike trips for each hr (24 points) both for east and west side
 rawcounts2 = gather(rawcounts,loc,val,East,West)
@@ -58,47 +65,105 @@ rawcounts3 = rawcounts3[,nameorder]
 head(rawcounts3)
 dim(rawcounts3)
 
-# Principal component analysis
+# Principal component analysis of the data where each row is a day and data for each day is the 24 hr bike counts on the
+# east and west side
 X = rawcounts3[,2:ncol(rawcounts3)]
 X = as.matrix(X)
 X[is.na(X)] = 0
 pca = prcomp(X)
 
+# Summary of the PCA
 summary(pca)
 screeplot(pca,type = "lines")
 
+# First two PC's are kept as in Jake's post (they account for 90% of the variance)
 scores = data.frame(pca$x[,1:2])
+
+# The scores are plotted on a 2D plot and each point (day) is colored based on total number of trips that day
+
 scores$tottrips = apply(X,1,sum)
 
+ggplot(data = scores,aes(x = PC1,y = PC2,color = tottrips)) + geom_point()
 
-ggplot(data = scores,aes(x = PC1,y = PC2,color = tottrips)) + geom_point() + theme_bw()
+# the color scheme in the plot in Jake's post (cubehelix) seems much nicer. 
+# But since I wasn't sure how to use it with continuous scale, I discretized total trips into buckets
+# and applied that color scheme
+CairoPNG("pcScores.png",height = 600, width = 600)
+ggplot(data = scores,aes(x = PC1,y = PC2,color = cut(tottrips,7))) + geom_point() + 
+  scale_color_manual(name = "total trips", values = cubeHelix(7)) 
+dev.off()
 
-loadings = pca$rotation[,1:2]
+# In Jake's post, loadings was not considered. Some of the subsequent inferences done using clustering can be found
+# from the loadings of the first 2 principal components (at least in this case)
+loadings = as.data.frame(pca$rotation[,1:2])
+loadings$label = row.names(loadings)
+loadings$loc = sapply(loadings$label,function(x) strsplit(x,"_")[[1]][1])
+loadings$hr = rep(seq(1,24),2)
 
-# clustering
+head(loadings)
+
+loadings2 = gather(loadings,pc,val,PC1,PC2)
+
+# the loadings plot below also shows what the clustering later shows that PC1 is about weekday bike riding patterns
+# and PC2 is about holiday bike riding patters
+# PC1 also shows what clustering shows later that West side peaks in the morning and East side peaks in the evening,
+CairoPNG("pcLoadings.png",height = 600, width = 800)
+ggplot(data = loadings2) + geom_line(aes(x = hr, y = val, color = loc)) + facet_grid(~pc,scales = "free_y") + theme_bw()
+dev.off()
+
+# clustering: Gaussian mixture model clustering done with two PC scores as in Jake's post
+#
 fitclus = Mclust(scores[,c("PC1","PC2")],G = 2)
 
-summary(fitclus)
-
+# append cluster information to scores data
 scores$clus = fitclus$classification
 
+# plot first 2 PC scores and color by cluster (the clustering in this case picks the two clusters seen visually)
 ggplot(data = scores,aes(x = PC1,y = PC2,color = clus)) + geom_point() + theme_bw()
 
+# append cluster information to the original data used for PCA (day x hr data set)
 rawcounts3$clus = scores$clus
 
 # merge cluster info back to original data
-
 rawcounts4 = inner_join(rawcounts,rawcounts3[,c("Date","clus")],by = "Date")
 
+# get the average number of bike rides for each cluster
 rawcounts5 = gather(rawcounts4[,c("Date","East","West","total","hr","clus")],loc,val,East,West,total)
 rawcounts6 = rawcounts5 %>% group_by(clus,loc,hr) %>% summarize(val = mean(val))
 
-ggplot(data = rawcounts6) + geom_line(aes(x = hr,y = val,color = loc)) + facet_grid(~clus) + theme_bw()
+#
+# This graph was used in the original post to show the difference in trend between two clusters
+# that led next to investigating the day of week in each cluster
+#
+CairoPNG("clusterAvg.png",height = 600, width = 800)
+ggplot(data = rawcounts6) + geom_line(aes(x = hr,y = val,color = loc)) + facet_grid(~clus) + 
+  ylab("Avg bike trips") + theme_bw()
+dev.off()
 
+# get weekday for each date
 scores$weekday = wday(rawcounts3$Date,label = TRUE)
 
+# plot 2 PC scores and color them by day of week
+# This plot gives interpretation to the clusters as weekday cluster (1) and weekend cluster (2)
+CairoPNG("scoresbyWkday.png",height = 600, width = 600)
 ggplot(data = scores,aes(x = PC1,y = PC2,color = weekday)) + geom_point() + theme_bw()
+dev.off()
 
-scores$friday = ifelse(scores$weekday == "Fri","Fri","Othr")
+# Next the few weekdays that fall with weekend cluster were investigated
+oddWkdays = data.frame(Date = rawcounts3$Date,clus = scores$clus,weekday = scores$weekday)
+oddWkdays = oddWkdays %>% filter(clus == 2,!(weekday %in% c("Sat","Sun")))
 
-ggplot(data = scores,aes(x = PC1,y = PC2,color = friday)) + geom_point() + theme_bw()
+# Get master holiday list (names) and associated dates in year 2012-2015
+mstrHolidayList = expand.grid(year = 2012:2015,holidayName = as.character(listHolidays()),stringsAsFactors = FALSE)
+HolidayDates = lapply(1:nrow(mstrHolidayList),function(x) holiday(year = mstrHolidayList$year[x], Holiday = mstrHolidayList$holidayName[x])@Data)
+mstrHolidayList$Date = do.call("c",lapply(HolidayDates,function(x) as.Date(x)))
+
+holidayList = holidayNYSE(2012:2015)
+holidayList = as.Date(holidayList)
+holidayName = mstrHolidayList$holidayName[match(holidayList,mstrHolidayList$Date)]
+holidayUS = data.frame(Date = holidayList,Name = holidayName)
+
+holidayList = c(holidayList,holidayList + 1,holidayList - 1)
+
+tmp = (as.Date(oddWkdays$Date) %in% holidayList)
+
