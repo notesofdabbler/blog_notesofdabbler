0ef05fc543ab9a1ddc694780eeabaa437327406e
rdabbler <dabbler110@gmail.com>
2015-10-16 22:33:49 -0400

    still on topic models

--- exploreAICHE2015_techProgram/getTopics.R
+++ exploreAICHE2015_techProgram/getTopics.R
@@ -4,9 +4,15 @@
 
 library(topicmodels)
 library(tm)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
 # load data
-load("talkdetaildf.Rda")
+load(file = "talksdf.Rda") # talks
+load("talkdetaildf.Rda") # talk abstracts
+affildf = read.csv("affildf_clean.csv",stringsAsFactors = FALSE) # list of organizations for each talk
+
 
 abstracts = talkdetaildf$abstract
 
@@ -35,6 +41,7 @@ table(termFreq)
 mincnt = 10
 dtm_m = as.matrix(dtm)
 dtm_m2 = dtm_m[,termFreq >= mincnt]
+dim(dtm_m2)
 
 termFreq_chk = colSums(dtm_m2)
 termFreq_chk = sort(termFreq_chk,decreasing = TRUE)
@@ -42,14 +49,20 @@ head(termFreq_chk,30)
 
 rowsum_chk = rowSums(dtm_m2)
 dtm_m2 = dtm_m2[rowsum_chk > 0,]
+dim(dtm_m2)
 
 numtopics = 10
 SEED = 2345
-tmp = LDA(dtm_m2, k = numtopics, control = list(seed = SEED))
-Terms = terms(tmp,20)
-Terms[,1:10]
+fittopic = LDA(dtm_m2, k = numtopics, control = list(seed = SEED))
+Terms = terms(fittopic,10)
+Terms[,1:numtopics]
 
-postprob = posterior(tmp)$topics
+topicnames = c("bio","crystallization","process design","HME","crystallization 2","dissolution","formulation",
+               "modeling","granulation","continuous")
+names(topicnames) = paste0("X",seq(1,10))
+
+postprob = posterior(fittopic)$topics
+
 
 likelytopic1 = rep(0,nrow(postprob))
 probtopic1 = rep(0,nrow(postprob))
@@ -70,3 +83,72 @@ topicdf = data.frame(likelytopic1 = likelytopic1,probtopic1 = probtopic1,
                      likelytopic2 = likelytopic2, probtopic2 = probtopic2)
 topicdf$id = as.numeric(row.names(dtm_m2))
 
+dfpostprob = data.frame(postprob)
+dfpostprob$id = as.numeric(row.names(dtm_m2))
+dfpostprob_g = gather(dfpostprob,topicnum,prob,-id)
+dfpostprob_g = inner_join(dfpostprob_g,talksdf[,c("id","session")],by = "id")
+
+dfpostprob_gsumm = dfpostprob_g %>% group_by(session,topicnum) %>% summarize(avgprob = mean(prob))
+dfpostprob_gsumm$topicname = topicnames[dfpostprob_gsumm$topicnum]
+
+dfpostprob_summ = spread(dfpostprob_gsumm,topicnum,avgprob)
+
+
+likelytopic1s = rep(0,nrow(dfpostprob_summ))
+probtopic1s = rep(0,nrow(dfpostprob_summ))
+likelytopic2s = rep(0,nrow(dfpostprob_summ))
+probtopic2s = rep(0,nrow(dfpostprob_summ))
+
+for(i in 1:nrow(dfpostprob_summ)){
+
+  print(i)  
+  getprobs = as.numeric(dfpostprob_summ[i,2:11])
+  probtopic1s[i] = max(getprobs)
+  likelytopic1s[i] = which(getprobs == probtopic1s[i])
+  probtopic2s[i] = max(getprobs[-likelytopic1s[i]])
+  likelytopic2s[i] = which(getprobs == probtopic2s[i])
+  
+}
+
+sessiontopics = data.frame(dfpostprob_summ$session, likelytopic1s = likelytopic1s,probtopic1s = probtopic1s, 
+                                     likelytopic2s = likelytopic2s, probtopic2s = probtopic2s)
+sessiontopics$top2prob = sessiontopics$probtopic1s + sessiontopics$probtopic2s
+
+ggplot(data = dfpostprob_gsumm) + geom_tile(aes(x = topicname, y = session, fill = avgprob)) + 
+  scale_fill_gradient(low = "white",high = "darkblue") + 
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
+dfpostprob_gaffil = inner_join(dfpostprob_g,affildf[,c("id","affil2")],by = "id")
+dfpostprob_gaffilsumm = dfpostprob_gaffil %>% group_by(affil2,topicnum) %>% summarize(avgprob = mean(prob))
+dfpostprob_affilsumm = spread(dfpostprob_gaffilsumm,topicnum,avgprob)
+
+topaffil = affildf %>% filter(affil2 != "Remove") %>% group_by(id, affil2) %>% summarize(cnt = n())
+topaffil2 = topaffil %>% group_by(affil2) %>% summarize(numtalks = n()) %>% arrange(desc(numtalks))
+
+dfpostprob_gaffilsumm2 = inner_join(dfpostprob_gaffilsumm,topaffil2[1:10,1],by = "affil2")
+
+ggplot(data = dfpostprob_gaffilsumm2) + geom_tile(aes(x = topicnum,y = affil2, fill = avgprob)) + 
+    scale_fill_gradient(low = "white",high = "darkblue") + theme_bw()
+
+# Output links to talks for each likely topic
+
+for(j in 1:numtopics){
+  topictalks = inner_join(topicdf,talksdf[,c("id","title","talkurl")],by = "id")
+  topictalks = topictalks %>% filter(likelytopic1 == j)
+  
+  outstr = as.character()
+  k = 1
+  for(i in 1:nrow(topictalks)){
+    title = topictalks$title[i]
+    talkurl = topictalks$talkurl[i]
+    outstr[k] = paste0("[",title,"](",talkurl,")   ")
+    k = k + 1
+    outstr[k] = paste0("Most likely topic ",topictalks$likelytopic1[i], "  Probability ",round(topictalks$probtopic1[i]*100),"%","  ")
+    k = k + 1
+    outstr[k] = paste0("2nd likely topic ",topictalks$likelytopic2[i], "  Probability ",round(topictalks$probtopic2[i]*100),"%","  \n\n")
+    k = k + 1
+  }
+  cat(paste(outstr,collapse = "\n"),file = paste0("linkFiles/topictalks",j,".Rmd"))
+  
+}
